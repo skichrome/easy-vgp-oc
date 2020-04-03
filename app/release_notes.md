62408d3abe4743071147970d06c596c7d7ffe69b
skichrome <toni.campeol@outlook.fr>
2020-04-03 17:18:45 +0200

    Updated release notes for app distribution and updated storage and cloud firestore path to match build type and product flavor (to avoid data loss).s

--- app/release_notes.md
+++ app/release_notes.md
@@ -1,2 +1,7 @@
 [Features]
- - Added performance monitoring sdk to the app, when the app crash the developers will be informed about why the crash happened.
+ - Added model, manufacturing year and  photo to machine. You can take a picture of your machine, it
+   will be used on pdf report.
+ - Added partially working feature 'generate report' on click on pdf button in report list.
+   The report is generated but you are not notified about the success or error of report generation
+   task. This task is done in the cloud so you must be connected to internet. 
+ - Updated comment alert dialog on vgp report generation.
--- app/src/main/java/com/skichrome/oc/easyvgp/util/Constants.kt
+++ app/src/main/java/com/skichrome/oc/easyvgp/util/Constants.kt
@@ -7,13 +7,13 @@ const val RC_SIGN_IN_CODE = 2000
 const val CURRENT_LOCAL_PROFILE = "current_profile_id_from_room"
 
 // Cloud Firestore and Cloud Stograge
-val REMOTE_ADMIN_COLLECTION = if (BuildConfig.DEBUG) "admin_debug" else "admin"
+val REMOTE_ADMIN_COLLECTION = if (BuildConfig.DEBUG) "admin_${BuildConfig.FLAVOR}_debug" else "admin_${BuildConfig.FLAVOR}"
 const val REMOTE_MACHINE_TYPE_DOCUMENT = "machine_type"
 const val REMOTE_MACHINE_TYPE_CONTROL_POINT_DOCUMENT = "machine_type_control_points"
 const val REMOTE_CONTROL_POINT_DOCUMENT = "control_points"
 const val REMOTE_VERSION = "1.0"
 
-val REMOTE_USER_COLLECTION = if (BuildConfig.DEBUG) "users_data_debug" else "users_data"
+val REMOTE_USER_COLLECTION = if (BuildConfig.DEBUG) "users_data_${BuildConfig.FLAVOR}_debug" else "users_data_${BuildConfig.FLAVOR}"
 const val REMOTE_REPORT_COLLECTION = "reports"
 
-val REMOTE_USER_STORAGE = if (BuildConfig.DEBUG) "users_data_debug" else "users_data"
+val REMOTE_USER_STORAGE = if (BuildConfig.DEBUG) "users_data_${BuildConfig.FLAVOR}_debug" else "users_data_${BuildConfig.FLAVOR}"
